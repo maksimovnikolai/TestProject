b4190281964db74bf19598248fcb646bdadfb053
Nikolai Maksimov <kerch.net@mail.ru>
2024-08-06 21:21:10 +0300

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Nikolai Maksimov on 06.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
