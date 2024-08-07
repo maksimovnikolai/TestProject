43f7caca49d506abe66b0479d892cf5313162040
Nikolai Maksimov <kerch.net@mail.ru>
2024-08-07 17:12:21 +0300

    чистый

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -8,35 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let label: UILabel = {
-        let label = UILabel()
-        return label
-    }()
-    
-    let textField: UITextField = {
-        let tf = UITextField()
-        return tf
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
     }
-    
-    func getSomeText() -> String {
-        "Hello world"
-    }
-    
-    func newFunc() {
-        // Лишний коммит
-    }
-    
-    // Исправил ошибки
-    
-    // еще что то добавили
-    
-    // еще немного изменений
-    
+
 }
 
