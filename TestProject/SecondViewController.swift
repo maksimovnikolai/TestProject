1a09fbb85bc7d13feb6ec12d48caa128017b7af1
Nikolai Maksimov <kerch.net@mail.ru>
2024-08-07 18:16:42 +0300

    add secondviewcontroller

---
+++ TestProject/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  TestProject
+//
+//  Created by Nikolai Maksimov on 07.08.2024.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
